98edf873d9dd71af5d10841fa0e0f462fd7af79a
danicoroman <2danicoroman@gmail.com>
2022-05-31 11:42:34 +0200

    Teoría acerca de las variables(var)

---
+++ 5.IntroduccionJS_en_curso/js/variables.js
@@ -0,0 +1,22 @@
+//Variables
+
+var producto = 'Audífonos Gamer'; //Iniciar variable y asignarle valor
+
+var disponible; //Iniciamos la variable pero sin valor
+
+producto = true; //Reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+//Estilos variables
+
+var nombre_producto = 'Monitor HD'; //underscore
+var nombreProducto = 'Monitor HD'; //CamelCase
+var NombreProducto = 'Monitor HD'; //Pascal Case
+var nombreproducto = 'Monitor HD'; //lowercase
+
+console.log(producto)
