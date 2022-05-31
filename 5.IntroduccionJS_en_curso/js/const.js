3c4f8b5b3dee1e794130fcd4b43eea6170cd652e
danicoroman <2danicoroman@gmail.com>
2022-05-31 14:59:57 +0200

    Variables let y const

---
+++ 5.IntroduccionJS_en_curso/js/const.js
@@ -0,0 +1,20 @@
+//Variables con const
+
+const producto = 'Audífonos Gamer'; //Iniciar variable y asignarle valor
+
+const disponible = true; //Iniciamos la variable pero sin valor
+
+producto = true; //Reasignando el valor de la variable
+
+disponible = true;
+
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+//Estilos variables
+
+const nombre_producto = 'Monitor HD'; //underscore
+const nombreProducto = 'Monitor HD'; //CamelCase
+const NombreProducto = 'Monitor HD'; //Pascal Case
+const nombreproducto = 'Monitor HD'; //lowercase
---
+++ 5.IntroduccionJS_en_curso/js/let.js
@@ -0,0 +1,20 @@
+//Variables con let
+
+let producto = 'Audífonos Gamer'; //Iniciar variable y asignarle valor
+
+let disponible; //Iniciamos la variable pero sin valor
+
+producto = true; //Reasignando el valor de la variable
+
+disponible = true;
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+//Estilos variables
+
+let nombre_producto = 'Monitor HD'; //underscore
+let nombreProducto = 'Monitor HD'; //CamelCase
+let NombreProducto = 'Monitor HD'; //Pascal Case
+let nombreproducto = 'Monitor HD'; //lowercase
