c7ce9920508d148a06dd583e25c2a95470c1d8cb
danicoroman <2danicoroman@gmail.com>
2022-06-01 11:48:35 +0200

    Introducción al método Math de JavaScript

---
+++ 5.IntroduccionJS_en_curso/js/math.js
@@ -0,0 +1,22 @@
+//Objeto Math
+
+
+let resultado;
+
+resultado = Math.PI;
+
+resultado = Math.round(2.5);
+resultado = Math.ceil(2.9); //Ceil siempre redondea hacia arriba
+resultado = Math.floor(2.9); //Floor siempre redondea hacia abajo
+resultado = Math.sqrt(144);
+resultado = Math.abs(-14);
+resultado = Math.min(-14, -63, 2, -9, 233);
+resultado = Math.max(-14, -63, 2, -9, 233);
+resultado = Math.random();
+resultado = Math.floor(Math.random() * 30);
+
+
+
+
+
+console.log(resultado);
