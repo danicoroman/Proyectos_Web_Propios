4a913ed93c04d427af32968cd2b48f172e32ade0
danicoroman <2danicoroman@gmail.com>
2022-06-01 11:31:28 +0200

    funciones con Strings

---
+++ 5.IntroduccionJS_en_curso/js/funcString.js
@@ -0,0 +1,22 @@
+//Strings o Cadenas de Texto
+
+const tweet = 'Aprendiendo JavaScript con el curso de Desarrollo Web Completo';
+const producto2 = 'Monitor HD';
+
+
+
+//Contar los caracteres de la cadena
+
+console.log(tweet.length);
+console.log(producto2);
+
+
+//IndexOf(retorna posición)
+
+console.log(tweet.indexOf('JavaScript'));
+console.log(producto2.indexOf('Tablet'));
+
+//Include (retorna true o false)
+
+console.log(tweet.includes('JavaScript'));
+console.log(producto2.includes('Tablet'));
