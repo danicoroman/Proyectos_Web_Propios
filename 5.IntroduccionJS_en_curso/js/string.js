713778be3f2fd444b38385c518bb584d891a8976
danicoroman <2danicoroman@gmail.com>
2022-06-01 10:29:15 +0200

    string.js añadido

---
+++ 5.IntroduccionJS_en_curso/js/string.js
@@ -0,0 +1,13 @@
+//Strings o Cadenas de Texto
+
+const producto = "Monitor de 20 Pulgadas";
+
+const producto2 = String('Monitor 30 Pulgadas'); //No Comunes
+const producto3 = new String('Monitor 50 Pulgadas'); //No comunes
+
+const producto4 = "Monitor de 20\""; //Escapar una comilla doble
+
+console.log(producto)
+console.log(producto2)
+console.log(producto3)
+console.log(producto4)
