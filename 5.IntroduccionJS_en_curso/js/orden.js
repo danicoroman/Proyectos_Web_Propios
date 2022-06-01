739170907d9beac66b2767fcde238d54c4a7ca7f
danicoroman <2danicoroman@gmail.com>
2022-06-01 12:22:49 +0200

    Orden de las  operaciones en JavaScript

---
+++ 5.IntroduccionJS_en_curso/js/orden.js
@@ -0,0 +1,29 @@
+//Orden de las operaciones
+
+let resultado;
+
+resultado = (20 + 30) * 2;
+
+resultado = (100 + 200 + 300) * 1.21; //Precio más IVA
+
+
+// console.log(resultado);
+
+//Incrementos
+
+let puntaje = 10;
+
+//Incremento en 1
+
+puntaje++; //10 (Primero muestra el valor y luego lo incrementa)
+
+++puntaje; //12 (Primero incrementa el valor y luego lo muestra)
+
+// puntaje--;
+
+
+puntaje = 10;
+
+puntaje += 10;
+
+console.log(puntaje);
