a690963b52af7200c4c9a745ac6759a9dfd71ec1
danicoroman <2danicoroman@gmail.com>
2022-06-01 12:35:59 +0200

    Como concatenar en JavaScript

---
+++ 5.IntroduccionJS_en_curso/js/concatena.js
@@ -0,0 +1,11 @@
+const nombre = 'Dani';
+const email = 'dani@dani.com';
+
+
+//Concatenación
+console.log(nombre + ' ' + email);
+
+
+//Template Strings - String Literals
+
+console.log(`Nombre Cliente: ${nombre} \nEmail: ${email}`);
