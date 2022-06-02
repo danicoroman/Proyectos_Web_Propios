0a0e936818b9082bfae136a7d4f79a6d23cf165c
danicoroman <2danicoroman@gmail.com>
2022-06-02 13:01:18 +0200

    Métodos principales para Arrays-JavaScript

--- 5.IntroduccionJS_en_curso/js/arrays.js
+++ 5.IntroduccionJS_en_curso/js/arrays.js
@@ -2,12 +2,7 @@
 
 const numeros = [10, 20, 30, 40, 50];
 
-console.table(numeros);
 
-
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
-
-console.table(meses);
 
 
 const array = ['Hola', 10, false, "no", null, { nombre: 'Daniel', trabajo: 'Programador' },
@@ -19,14 +14,39 @@ const array = ['Hola', 10, false, "no", null, { nombre: 'Daniel', trabajo: 'Prog
 
 //Acceder a los valores de un array
 
-console.log(numeros[4]);
-console.log(numeros[2]);
+// console.log(numeros[4]);
+// console.log(numeros[2]);
 
 //Conocer la longitud del Array
 
-console.log(meses.length);
+// console.log(meses.length);
 
 
-meses.forEach(function(mes) {
-    console.log(mes);
-})
+// meses.forEach(function(mes) {
+//     console.log(mes);
+// })
+
+
+numeros.push(2, 22, 222); //Agrega al final del array
+
+
+numeros.unshift(-10, -11, -12) //Agrega al Principio del array
+
+console.table(numeros);
+
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+
+// meses.pop(); //Elimina el último elemento del Array
+// meses.shift(); //Elimina el primer elemento del Array
+
+// meses.splice(2, 1)
+
+
+// console.table(meses);
+
+//Rest Operator o Spread Operator
+
+// const nuevoArray = [...meses, 'Junio']
+const nuevoArray = ['Diciembre', ...meses]
+console.log(nuevoArray);
