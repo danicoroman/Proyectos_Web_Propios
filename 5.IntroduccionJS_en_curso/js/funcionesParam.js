e8b2329b0cd392045d7b5ef064a7f36581466d83
danicoroman <2danicoroman@gmail.com>
2022-06-03 11:26:17 +0200

    Funciones con parámetros en JavaScript

---
+++ 5.IntroduccionJS_en_curso/js/funcionesParam.js
@@ -0,0 +1,16 @@
+function sumar(numero1 = 0, numero2 = 0) { //numero1, 2 son parámetros =0 parámetros por default si falta algún valor, tomará el valor de 0
+    console.log(numero1 + numero2);
+}
+
+sumar(10, 10); //Argumentos o los valores reales
+sumar(3, 3); //Argumentos o los valores reales
+sumar(3, 6); //Argumentos o los valores reales
+sumar(1, 2); //Argumentos o los valores reales
+
+
+
+
+const sumar2 = function(n1, n2) {
+    console.log(n1 + n2);
+}
+sumar2(5, 10);
