eb8afda876dbec6bbb4a28f86d16ac26c9bed243
danicoroman <2danicoroman@gmail.com>
2022-06-03 14:40:52 +0200

    Bucle For en JavaScript

---
+++ 5.IntroduccionJS_en_curso/js/bucles.js
@@ -0,0 +1,40 @@
+//For Loop
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(`El numero ${i} es par`);
+//     } else {
+//         console.log(`El número ${i} es impar`);
+//     }
+// }
+
+const carrito = [
+    { nombre: 'Monitor 20 Pulgadas', precio: 500 },
+    { nombre: 'Televisión 50 Pulgadas', precio: 700 },
+    { nombre: 'Tablet', precio: 300 },
+    { nombre: 'Audífonos', precio: 200 },
+    { nombre: 'Teclado', precio: 20 },
+    { nombre: 'Móvil', precio: 250 },
+    { nombre: 'Altavoces', precio: 150 },
+    { nombre: 'Portátil', precio: 850 }
+];
+
+for (let i = 0; i < carrito.length; i++) {
+    console.log(carrito[i].nombre);
+
+}
+
+
+//While Loop
+
+
+
+
+
+
+
+//Do While Loop
