7139217cbb5effd55795bffd20b1b61c63921f18
danicoroman <2danicoroman@gmail.com>
2022-06-05 13:13:25 +0200

    This en JavaScript

--- 5.IntroduccionJS_en_curso/js/this.js
+++ 5.IntroduccionJS_en_curso/js/this.js
@@ -5,7 +5,7 @@ const reserva = {
     apellido: 'Corona',
     total: 2000,
     pagado: false,
-    informacion: function() { //Hay que usar function en vez de un arrow function
+    informacion: function() { //Hay que usar function en vez de un arrow functiona
         console.log(`El Cliente ${this.nombre} reservó y su cantidad a pagar es de ${this.total} €`);
     }
 }
