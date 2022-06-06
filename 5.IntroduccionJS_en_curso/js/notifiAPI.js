16356efa2663c5459b387c729a259b77dccdc426
danicoroman <2danicoroman@gmail.com>
2022-06-06 14:58:46 +0200

    Enviar notificaciones por la clase Notification de JavaScript

---
+++ 5.IntroduccionJS_en_curso/js/notifiAPI.js
@@ -0,0 +1,17 @@
+const boton = document.querySelector('#boton');
+
+boton.addEventListener('click', function() {
+    Notification.requestPermission()
+        .then(resultado => console.log('El resultado es: ', resultado))
+})
+
+
+
+
+
+if (Notification.permission == 'granted') {
+    new Notification('Esta es una notificación', {
+        icon: 'img/ccj.png',
+        body: 'DaniCorochan'
+    })
+}
