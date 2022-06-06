07c2ed0ae2541c3ee8cedbfd708d35601aa3f91a
danicoroman <2danicoroman@gmail.com>
2022-06-06 12:45:02 +0200

    Prototype para formatear los datos de un objeto en JavaScript

--- 5.IntroduccionJS_en_curso/js/objectConstruct.js
+++ 5.IntroduccionJS_en_curso/js/objectConstruct.js
@@ -11,11 +11,23 @@ const producto = {
 function Producto(nombre, precio) {
     this.nombre = nombre;
     this.precio = precio;
+}
 
+//Crear funciones que sólo se utilizan en un objeto en específico
+
+Producto.prototype.formatearProducto = function() {
+    return `El Producto ${this.nombre} tiene un precio de: ${this.precio} €`
+}
+
+
+function formatearProducto(producto) {
+    return `El Producto ${producto.nombre} tiene un precio de: ${producto.precio} €`
 }
 
 const producto2 = new Producto('Monitor Curvo de 49"', 500);
 const producto3 = new Producto('Portatil', 300);
 
-console.log(producto2);
-console.log(producto3);
+
+
+console.log(producto2.formatearProducto());
+console.log(producto3.formatearProducto());
