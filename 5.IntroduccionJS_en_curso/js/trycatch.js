8b1f287b187b84a03249ac2b47cf05286150281f
danicoroman <2danicoroman@gmail.com>
2022-06-06 13:26:53 +0200

    Try/Catch para capturar errores  en JavaScript

---
+++ 5.IntroduccionJS_en_curso/js/trycatch.js
@@ -0,0 +1,13 @@
+const numero1 = 20;
+const numero3 = 30;
+
+console.log(numero1);
+
+try {
+    console.log(numero2); //Hace que al dar error no se detenga el programa pero muestre el error
+} catch (error) {
+    console.log(error);
+}
+
+
+console.log(numero3);
