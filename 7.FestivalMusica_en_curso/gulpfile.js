4711cbc7174c0a51e5934f0e6c692d458dc9078c
danicoroman <2danicoroman@gmail.com>
2022-06-10 14:52:37 +0200

    Convertidor de webp y optimizador de imágenes en gulp

--- 7.FestivalMusica_en_curso/gulpfile.js
+++ 7.FestivalMusica_en_curso/gulpfile.js
@@ -1,6 +1,13 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+
+//Imágenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css(done) {
     //Identificar el archivo SASS-src
@@ -13,10 +20,36 @@ function css(done) {
 }
 
 function dev(done) {
+
     watch('src/scss/**/*.scss', css);
 
     done();
 }
 
+function versionWebp(done) {
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+
+    done();
+}
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'));
+
+
+    done();
+}
+
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev);
