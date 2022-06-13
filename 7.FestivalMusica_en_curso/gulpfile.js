5ab078ae61e41a669b7663fd2c982d072abe0f24
danicoroman <2danicoroman@gmail.com>
2022-06-13 14:14:16 +0200

    Line Up de artistas-finalizado

--- 7.FestivalMusica_en_curso/gulpfile.js
+++ 7.FestivalMusica_en_curso/gulpfile.js
@@ -8,6 +8,7 @@ const plumber = require('gulp-plumber');
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     //Identificar el archivo SASS-src
@@ -49,7 +50,19 @@ function imagenes(done) {
     done();
 }
 
+function versionAvif(done) {
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
+
+    done();
+}
+
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(imagenes, versionWebp, dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
