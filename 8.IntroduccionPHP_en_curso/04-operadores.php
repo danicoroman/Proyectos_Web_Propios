e4f0c07f8c244ce07132c355ccea8c10c93f853c
danicoroman <2danicoroman@gmail.com>
2022-06-17 13:47:05 +0200

    Operadores y Comparadores en PHP

--- 8.IntroduccionPHP_en_curso/04-operadores.php
+++ 8.IntroduccionPHP_en_curso/04-operadores.php
@@ -1,5 +1,28 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
+
+//Sumar
+echo $numero1 + $numero2;
+
+//Resta
+echo '<br>' .$numero1 - $numero2;
+
+//Multiplicar
+echo '<br>';
+echo $numero1 * $numero2;
+
+//Dividir
+echo $numero1 / $numero2;
+
+//Multiplicar cierta cantidad de veces
+echo 2**3;
+
+
+
+
+
 
 
 
--- 8.IntroduccionPHP_en_curso/05-comparacion.php
+++ 8.IntroduccionPHP_en_curso/05-comparacion.php
@@ -1,5 +1,44 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 30;
+$numero3 = 30;
+$numero4 = '30';
+
+var_dump($numero1 > $numero2);
+echo '<br>';
+
+var_dump($numero1 < $numero2);
+echo '<br>';
+
+var_dump($numero1 >= $numero2);
+echo '<br>';
+
+var_dump($numero1 <= $numero2);
+echo '<br>';
+
+var_dump($numero3 == $numero2);
+echo '<br>';
+
+var_dump($numero4 == $numero2);
+echo '<br>';
+
+var_dump($numero4 === $numero2);
+echo '<br>';
+
+
+//-1 Si Izquierda es menor
+var_dump($numero1 <=> $numero2);
+echo '<br>';
+
+//0 Si es igual
+
+var_dump($numero2 <=> $numero3);
+echo '<br>';
+
+//1 Si Izquierda es mayor
+var_dump($numero2 <=> $numero1);
+
 
 
 
