08ebb1f70491901cf9948fc4fbf699d2e9a250d5
danicoroman <2danicoroman@gmail.com>
2022-06-17 15:11:23 +0200

    Incrementos Decrementos y Strings en PHP

--- 8.IntroduccionPHP_en_curso/06-inc-dec.php
+++ 8.IntroduccionPHP_en_curso/06-inc-dec.php
@@ -1,5 +1,13 @@
 <?php include 'includes/header.php';
+$numero1 = 30;
 
+$numero1++;
+
+echo ++$numero1;
+
+$numero2 = 30;
+
+echo --$numero2;
 
 
 
--- 8.IntroduccionPHP_en_curso/07-string-methods.php
+++ 8.IntroduccionPHP_en_curso/07-string-methods.php
@@ -1,5 +1,38 @@
 <?php include 'includes/header.php';
 
+$nombreCliente ='Daniel     ';
+
+
+//Conocer longitus de un String
+echo strlen($nombreCliente);
+
+//Eliminar espacios en blanco
+$texto= trim($nombreCliente);
+echo strlen( $texto);
+echo  $texto;
+
+//Convertirlo a Mayúsculas
+
+echo strtoupper($nombreCliente);
+
+//Convertirlo a Minúsculas
+
+echo strtolower($nombreCliente);
+
+echo str_replace('Dani', 'Tom', $nombreCliente);
+
+//revisar si un string existe o no
+
+echo strpos($nombreCliente,'Dan');
+
+
+$tipoCliente = 'Premium';
+echo '<br>';
+
+echo 'El cliente ' . $nombreCliente . ' es ' . $tipoCliente;
+
+echo "El Cliente $nombreCliente es $tipoCliente";
+
 
 
 
