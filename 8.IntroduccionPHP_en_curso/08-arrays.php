2f5035812536d7ad98c3ac74e93496661b90b85c
danicoroman <2danicoroman@gmail.com>
2022-06-17 15:19:02 +0200

    Arrays en PHP

--- 8.IntroduccionPHP_en_curso/08-arrays.php
+++ 8.IntroduccionPHP_en_curso/08-arrays.php
@@ -1,5 +1,14 @@
 <?php include 'includes/header.php';
 
+$carrito = ['Tablet', 'Televisión', 'Computadora'];
+
+//Util para ver los contenidos de un Array
+echo '<pre>';
+var_dump($carrito);
+echo '</pre>';
+
+//Acceder a un elemento del Array
+echo $carrito[1];
 
 
 
