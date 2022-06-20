fee7ec1089ac58e16019ef8cb362e28b382a6f75
danicoroman <2danicoroman@gmail.com>
2022-06-20 14:30:32 +0200

    Bucles y ForEach en PHP

--- 8.IntroduccionPHP_en_curso/13-loops.php
+++ 8.IntroduccionPHP_en_curso/13-loops.php
@@ -1,6 +1,65 @@
 <?php include 'includes/header.php';
 
+//While
+$i = 0; //Iterador
 
+while($i < 10){
+    // echo $i .'<br>';
+
+    $i++; //Incremento
+}
+
+echo '<br>';
+//Do While
+
+$i =0;
+
+do {
+    // echo $i .'<br>';
+   
+   $i++; 
+} while ($i < 10);
+
+// echo '<br>';
+
+
+//For Loop
+
+for ($i=1; $i < 100; $i++) { 
+    if ($i % 3 ==0 && $i % 5 ==0) {
+        echo $i . '-Fizz Buzz <br>';
+
+    }
+    elseif($i % 3 == 0){
+        echo $i . '-Fizz <br>';
+    } elseif ($i % 5 == 0) {
+        echo $i . '-Buzz <br>';
+
+    }
+}
+
+
+
+//For Each Loop
+$clientes = array('Dani', 'Juan', 'Karen');
+
+foreach ($clientes as $cliente) {
+    echo $cliente . '<br>';
+}
+
+
+echo count($clientes);
+
+
+$cliente = [
+    'nombre' => 'Dani',
+    'saldo' => 200,
+    'tipo' => 'Premium'  
+];
+echo '<br>';
+foreach ($cliente as $key => $valor) {
+    echo $key . "-" . $valor . '<br>';
+}
 
 
 include 'includes/footer.php';
--- 8.IntroduccionPHP_en_curso/14-foreach.php
+++ 8.IntroduccionPHP_en_curso/14-foreach.php
@@ -1,5 +1,38 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Televisión 24"',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor Curvo',
+        'precio' => 400,
+        'disponible' => false
+    ]
+    ];
+
+    foreach ($productos as $producto) { ?>
+    <li>
+        <p>Producto:  <?php echo $producto['nombre']; ?> </p>
+        <p>Precio:  <?php echo $producto['precio'] .' €'; ?> </p>
+        <p><?php echo ($producto['disponible']) ? 'Disponible' : 'No disponible'; ?> </p>
+        
+    </li>
+
+       
+    <?php 
+    // echo '<pre>';
+    // var_dump($producto);
+    // echo '</pre>';
+}
+
 
 
 
