bb213b1f4b0c21e4c1e345bfff5945f7fe946f02
danicoroman <2danicoroman@gmail.com>
2022-06-20 11:51:44 +0200

    Condicionales y Switch en PHP

--- 8.IntroduccionPHP_en_curso/11-funciones-arrays.php
+++ 8.IntroduccionPHP_en_curso/11-funciones-arrays.php
@@ -1,6 +1,40 @@
 <?php include 'includes/header.php';
 
+//in_array - buscar elementos en un Array
 
+$carrito = ['Tablet', 'Computadora', 'Televisión'];
+
+var_dump(in_array('Tablet', $carrito));
+var_dump(in_array('Audífonos', $carrito));
+
+
+
+//Ordenar elementos de un array
+$numeros = array(1,3,4,5,1,2);
+sort($numeros); // de menor a mayor
+rsort($numeros); // de mayor a menor
+
+
+echo '<pre>';
+var_dump($numeros);
+echo '</pre>';
+
+
+//Ordenar Array asociativo
+$cliente = [
+    'saldo' => 200,
+    'tipo' => 'Premium',
+    'nombre' => 'Dani'
+];
+
+asort($cliente); //Ordena por valores (Orden alfabético)
+arsort($cliente); //Ordena por valores (Orden alfabético inverso)
+ksort($cliente); //Ordena por llaves (Orden alfabético)
+krsort($cliente); //Ordena por llaves (Orden alfabético inverso)
+
+echo '<pre>';
+var_dump($cliente);
+echo '</pre>';
 
 
 
--- 8.IntroduccionPHP_en_curso/12-condicionales.php
+++ 8.IntroduccionPHP_en_curso/12-condicionales.php
@@ -1,5 +1,74 @@
 <?php include 'includes/header.php';
 
+$autenticado = false;
+$admin = true;
+
+if($autenticado || $admin){
+    echo 'Usuario autenticado correctamente';
+}else{
+    echo 'Usuario no autenticado, inicia sesión';
+}
+
+
+//If anidados...
+
+$cliente = [
+    'nombre' => 'Dani',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo'=>'Premium'
+    ]   
+];
+
+if(!empty($cliente)){
+    echo 'El array de cliente no está vacío';
+
+    if($cliente['saldo'] > 0)
+    {
+        echo '<br>';
+        echo 'El Cliente tiene Saldo disponible';
+    }
+    else{
+        echo '<br>';
+        echo 'No hay saldo';
+    }
+}
+
+//Else if
+echo '<br>';
+
+if($cliente['saldo'] > 0){
+    echo 'El Cliente tiene saldo';
+}elseif ($cliente['informacion']['tipo'] === 'Premium') {
+    echo 'El cliente es Premium';
+}else {
+    echo 'No hay cliente premium o con saldo';
+}
+
+//Switch
+
+echo '<br>';
+
+
+$tecnologia = 'HTML';
+
+switch ($tecnologia) {
+    case 'PHP':
+        echo 'PHP, un excelente lenguaje';
+        break;
+    case 'JavaScript':
+        echo 'El lenguaje de la web';
+        break;
+    case 'HTML':
+        echo 'No es un lenguaje de programación';
+        break;
+    
+    default:
+    echo 'Algún lenguaje que no sé cuál es';
+
+        break;
+}
+
 
 
 
