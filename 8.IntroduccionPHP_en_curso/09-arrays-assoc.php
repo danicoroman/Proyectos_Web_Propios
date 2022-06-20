173ecfdd31756bbeb123a3015945ab468cb26901
danicoroman <2danicoroman@gmail.com>
2022-06-20 11:03:05 +0200

    Arrays asociativos en PHP

--- 8.IntroduccionPHP_en_curso/09-arrays-assoc.php
+++ 8.IntroduccionPHP_en_curso/09-arrays-assoc.php
@@ -1,6 +1,26 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Dani',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'premium'
+    ]
+];
 
+echo '<pre>';
+var_dump($cliente);
+echo '</pre>';
+
+// echo $cliente['nombre'];
+// echo $cliente['informacion']['tipo'];
+
+$cliente['codigo'] = 12320241354;
+
+
+echo '<pre>';
+var_dump($cliente);
+echo '</pre>';
 
 
 include 'includes/footer.php';
