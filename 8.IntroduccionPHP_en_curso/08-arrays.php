7a29cd9edcc5dc3f9e64922400ad590386e65789
danicoroman <2danicoroman@gmail.com>
2022-06-20 10:51:52 +0200

    Arrays y funciones en PHP

--- 8.IntroduccionPHP_en_curso/08-arrays.php
+++ 8.IntroduccionPHP_en_curso/08-arrays.php
@@ -2,13 +2,25 @@
 
 $carrito = ['Tablet', 'Televisión', 'Computadora'];
 
+
+//Acceder a un elemento del Array
+echo $carrito[1];
+
+//Añade un elemento en el índice 3 del array
+$carrito[3] = 'Nuevo Producto...';
+
+//Añadir un elemento al final del array
+array_push($carrito, 'Audífonos');
+
+//Añadir un elemento al inicio del array
+array_unshift($carrito, 'Tablet 2');
+
+
 //Util para ver los contenidos de un Array
 echo '<pre>';
 var_dump($carrito);
 echo '</pre>';
 
-//Acceder a un elemento del Array
-echo $carrito[1];
 
 
 
