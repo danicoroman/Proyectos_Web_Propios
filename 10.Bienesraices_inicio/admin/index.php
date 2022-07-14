64277c0a01340cce5ef91b91e50030b6c9c31292
danicoroman <2danicoroman@gmail.com>
2022-07-14 13:57:25 +0200

    tabla para previsualizar las propiedades de la base de datos(HTML y estilos)

--- 10.Bienesraices_inicio/admin/index.php
+++ 10.Bienesraices_inicio/admin/index.php
@@ -14,6 +14,31 @@ incluirTemplate('header');
 
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td><img src="../src/img/anuncio1.jpg" class="imagen-tabla"></td>
+                    <td>12000€</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
     <?php incluirTemplate('footer'); ?>
